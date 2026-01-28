fe5b64705528dfd666a3cf5aa57149f2808beba4
sateesh <sateesh1417@gmail.com>
2026-01-28 21:07:35 +0000

    new change

--- 05.commandop.sh
+++ 05.commandop.sh
@@ -5,9 +5,10 @@ echo "$TIMESTAMP"
 FT=$(date +%s)
 echo "Starting time is $FT"
 sleep 20 &
+
 ST=$(date +%s)
 echo "Ending time is $ST"
 
-DT=$ ($ST-$FT)
+DT=$(($ST-$FT))
 echo " Difference in time is $DT"
 

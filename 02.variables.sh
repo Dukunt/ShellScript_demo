39545e4aa70ec8780b8d7ae054c0f243b69c07f8
sateesh <sateesh1417@gmail.com>
2026-01-28 19:55:35 +0000

    new change

--- 02.variables.sh
+++ 02.variables.sh
@@ -1,7 +1,7 @@
-#! /bin/bash
-$STU=Sateesh
-$TUT=Shiva
+#!/bin/bash
+ STU=Sateesh
+ TUT=Shiva
 
-echo " $STU: Hi $TUT How are you?"
-echo "$TUT: I am fine $STU. thanks . How are u?"
-echo "$STU: fine $TUT."
+ echo " $STU: Hi $TUT How are you?"
+ echo "$TUT: I am fine $STU. thanks . How are u?"
+ echo "$STU: fine $TUT."

8d4301a27ab64ea6f951ed305e3121f54a84fe60
sateesh <sateesh1417@gmail.com>
2026-01-29 16:45:12 +0000

    new change

--- 08.Functions.sh
+++ 08.Functions.sh
@@ -4,10 +4,10 @@ read $x
 echo "Enter the second number"
 read $y
 
-TOTAL() {
-    VALUE=$(($x+$y))
+ TOTAL() {
+    VALUE=$(($x + $y))
     echo "sum of the two numbers is $VALUE "
-}
+ }
 
 TOTAL
 

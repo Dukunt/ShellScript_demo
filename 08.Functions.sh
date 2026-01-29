e7e2082eb5cff33b5de70209f9e29e1bed3345c5
sateesh <sateesh1417@gmail.com>
2026-01-29 18:45:27 +0000

    New change

--- 08.Functions.sh
+++ 08.Functions.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
-echo "Enter first the number"
-read x
-echo "Enter the second number"
-read y
-TOTAL(){
-    VALUE=$(($x+$y))
-    echo "sum of the two numbers is $VALUE "
+USERID=$(id -u)
+if [ $USERID != 0 ] ; then
+ echo " For any installation use Sudo user"
+ exit 1
+fi 
+
+STATUS() {
+    if [$1 !=0 ] ; then
+    echo " $2.... FAILURE"
+    else
+    echo "$2.... SUCCESS"
 }
-TOTAL
+
+STATUS $? Nginx installation
+
+

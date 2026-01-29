d6bc34734fb0054e45fbd71604f6343813cbaa61
sateesh <sateesh1417@gmail.com>
2026-01-29 17:19:52 +0000

    new change

--- 08.Functions.sh
+++ 08.Functions.sh
@@ -5,10 +5,3 @@ echo "Enter the second number"
 read y
 VALUE=$(($x+$y))
 echo "sum of the two numbers is $VALUE "
-
- echo " Enter the first number"
- read FNUM
- echo "Enter the second number"
- read SNUM
- TOTAL=$(($FNUM+$SNUM))
- echo " Sum of the two numbers is $TOTAL"

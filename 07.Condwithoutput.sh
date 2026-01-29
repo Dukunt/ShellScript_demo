4533d17f17498db86ca734ce6f39d8fd25e988b3
sateesh <sateesh1417@gmail.com>
2026-01-29 09:38:30 +0000

    new change

---
+++ 07.Condwithoutput.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+ USER=$(id -u)
+ if [$USER != 0] ; then
+  echo " please use root user for any installation"
+  exit 1
+  else
+  echo "Installation will be done"
+  dnf install nginx -y
+fi
+
+if  [$? = 0 ]; then
+ echo "Nginx installation was successful"
+ else
+ echo "Installation failed"

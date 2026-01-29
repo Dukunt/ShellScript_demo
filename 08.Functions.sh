d8ced3c2bbb9eeda45ada03f2c5ea19f08daa9a0
sateesh <sateesh1417@gmail.com>
2026-01-29 16:31:49 +0000

    new change

---
+++ 08.Functions.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo " Enter the number below 100"
+read $x
+echo "Enter the second number"
+read $y
+
+TOTAL() {
+    VALUE=$(($x+$y))
+    echo "sum of the two numbers is $VALUE "
+}
+
+TOTAL
+
